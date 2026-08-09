ae61b7167bcdba302cb8ecf8eeb2912a81d7c7ab
lninolagidze-hub <lninolagidze@gmail.com>
2026-08-09 16:16:20 +0400

    Lesson 40 Django Class Based Views

--- Sunamoebi/urls.py
+++ Sunamoebi/urls.py
@@ -3,32 +3,49 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('category/<int:category_id>/', views.category_products, name='category_products'),
-    path('discounted/', views.discounted_products, name='discounted_products'),
+
+    path(
+        '',
+        views.HomeView.as_view(),
+        name='home'
+    ),
+
+    path(
+        'category/<int:category_id>/',
+        views.CategoryProductsView.as_view(),
+        name='category_products'
+    ),
+
+    path(
+        'discounted/',
+        views.DiscountedProductsView.as_view(),
+        name='discounted_products'
+    ),
+
     path(
         'product/<int:product_id>/',
-        views.product_detail,
+        views.ProductDetailView.as_view(),
         name='product_detail'
     ),
+
     path(
-    'add-product/',
-    views.add_product,
-    name='add_product'
+        'product/add/',
+        views.AddProductView.as_view(),
+        name='add_product'
     ),
-    
-        path(
-        'update-product/<int:product_id>/',
-        views.update_product,
+
+    path(
+        'product/<int:product_id>/update/',
+        views.UpdateProductView.as_view(),
         name='update_product'
     ),
 
     path(
-    'delete-product/<int:product_id>/',
-    views.delete_product,
-    name='delete_product'
-)
-]
+        'product/<int:product_id>/delete/',
+        views.DeleteProductView.as_view(),
+        name='delete_product'
+    ),
 
+]
 
 
--- Sunamoebi/views.py
+++ Sunamoebi/views.py
@@ -1,115 +1,198 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Count
+from django.views import View
+
 from .models import Product, Category
 from .forms import ProductForm
 
 
-def home(request):
-    products = Product.objects.all().order_by('price')
+class HomeView(View):
 
-    categories = Category.objects.annotate(
-        product_count=Count('product')
-    ).filter(product_count__gt=0)
+    def get(self, request):
 
-    return render(
-        request,
-        'Sunamoebi/home.html',
-        {
-            'products': products,
-            'categories': categories,
-        }
-    )
-    
-def category_products(request, category_id):
-    category = Category.objects.get(id=category_id)
-    products = Product.objects.filter(category=category).order_by('price')
+        products = Product.objects.all().order_by('price')
 
-    return render(
-        request,
-        'Sunamoebi/category_products.html',
-        {
-            'category': category,
-            'products': products,
-        }
-    )
-    
-def discounted_products(request):
-    products = Product.objects.filter(
-        has_discount=True
-    ).order_by('price')
+        categories = Category.objects.annotate(
+            product_count=Count('product')
+        ).filter(
+            product_count__gt=0
+        )
 
-    return render(
-        request,
-        'Sunamoebi/discounted_products.html',
-        {
-            'products': products,
-        }
-    )
+        return render(
+            request,
+            'Sunamoebi/home.html',
+            {
+                'products': products,
+                'categories': categories,
+            }
+        )
 
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
 
-    return render(
-        request,
-        'Sunamoebi/product_detail.html',
-        {
-            'product': product,
-        }
-    )
+class CategoryProductsView(View):
 
-def add_product(request):
-    if request.method == 'POST':
+    def get(self, request, category_id):
+
+        category = get_object_or_404(
+            Category,
+            id=category_id
+        )
+
+        products = Product.objects.filter(
+            category=category
+        ).order_by('price')
+
+        return render(
+            request,
+            'Sunamoebi/category_products.html',
+            {
+                'category': category,
+                'products': products,
+            }
+        )
+
+
+class DiscountedProductsView(View):
+
+    def get(self, request):
+
+        products = Product.objects.filter(
+            has_discount=True
+        ).order_by('price')
+
+        return render(
+            request,
+            'Sunamoebi/discounted_products.html',
+            {
+                'products': products,
+            }
+        )
+
+
+class ProductDetailView(View):
+
+    def get(self, request, product_id):
+
+        product = get_object_or_404(
+            Product,
+            id=product_id
+        )
+
+        return render(
+            request,
+            'Sunamoebi/product_detail.html',
+            {
+                'product': product,
+            }
+        )
+
+
+class AddProductView(View):
+
+    def get(self, request):
+
+        form = ProductForm()
+
+        return render(
+            request,
+            'Sunamoebi/add_product.html',
+            {
+                'form': form,
+            }
+        )
+
+    def post(self, request):
+
         form = ProductForm(request.POST)
 
         if form.is_valid():
             form.save()
             return redirect('home')
 
-    else:
-        form = ProductForm()
+        return render(
+            request,
+            'Sunamoebi/add_product.html',
+            {
+                'form': form,
+            }
+        )
 
-    return render(
-        request,
-        'Sunamoebi/add_product.html',
-        {
-            'form': form,
-        }
-    )
 
-def update_product(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
+class UpdateProductView(View):
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
+    def get(self, request, product_id):
+
+        product = get_object_or_404(
+            Product,
+            id=product_id
+        )
+
+        form = ProductForm(
+            instance=product
+        )
+
+        return render(
+            request,
+            'Sunamoebi/update_product.html',
+            {
+                'form': form,
+                'product': product,
+            }
+        )
+
+    def post(self, request, product_id):
+
+        product = get_object_or_404(
+            Product,
+            id=product_id
+        )
+
+        form = ProductForm(
+            request.POST,
+            instance=product
+        )
 
         if form.is_valid():
             form.save()
-            return redirect('product_detail', product_id=product.id)
 
-    else:
-        form = ProductForm(instance=product)
+            return redirect(
+                'product_detail',
+                product_id=product.id
+            )
 
-    return render(
-        request,
-        'Sunamoebi/update_product.html',
-        {
-            'form': form,
-            'product': product,
-        }
-    )
-    
+        return render(
+            request,
+            'Sunamoebi/update_product.html',
+            {
+                'form': form,
+                'product': product,
+            }
+        )
 
-def delete_product(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
 
-    if request.method == 'POST':
+class DeleteProductView(View):
+
+    def get(self, request, product_id):
+
+        product = get_object_or_404(
+            Product,
+            id=product_id
+        )
+
+        return render(
+            request,
+            'Sunamoebi/delete_product.html',
+            {
+                'product': product,
+            }
+        )
+
+    def post(self, request, product_id):
+
+        product = get_object_or_404(
+            Product,
+            id=product_id
+        )
+
         product.delete()
-        return redirect('home')
 
-    return render(
-        request,
-        'Sunamoebi/delete_product.html',
-        {
-            'product': product,
-        }
-    )
+        return redirect('home')
